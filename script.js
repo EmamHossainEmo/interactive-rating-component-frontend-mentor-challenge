b2b4d4cbce1ffcef87af13b269146d176e341560
EmamHossainEmo <136217471+EmamHossainEmo@users.noreply.github.com>
2023-09-26 21:32:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const ratingSection = document.querySelector("#rating-section");
+const thankingSection = document.querySelector("#thanking-section");
+const btn = document.querySelector("#submit-btn");
+btn.addEventListener("click", changeInterface);
+ratingSection.style.display = "flex";
+thankingSection.style.display = "none";
+
+function changeInterface() {
+  ratingSection.style.display = "none";
+  thankingSection.style.display = "flex";
+  document.querySelector("#given-rating").innerText = getRating();
+}
+
+function getRating() {
+  for (let i = 0; i < 5; i++) {
+    if (document.getElementsByTagName("input")[i].checked) {
+      return document.getElementsByTagName("input")[i].value;
+    }
+  }
+}
